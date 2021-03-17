bf4ed6e212807b203ecddcadcd25a06f697bdcdf
fekete20 <Antal@DESKTOP>
2021-03-17 20:06:06 +0100

    Trivial example project for presentating junit.

---
+++ Calculator/src/main/CalculatorLogic.java
@@ -0,0 +1,25 @@
+package main;
+
+public class CalculatorLogic {
+	
+	public double add(double a, double b) {
+		return a + b;
+	}
+	
+	public double subtract(double a, double b) {
+		return a - b;
+	}
+	
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+	
+	public double divide(double a, double b) {
+		if(b == 0) {
+			throw new IllegalArgumentException("Nullával nem kéne osztani");
+		} else {
+			return a / b;
+		}
+	}
+
+}
